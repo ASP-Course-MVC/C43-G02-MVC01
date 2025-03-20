b3d0204d13a70e047df334783c4f705aa7559015
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-03-21 01:13:16 +0200

    Upgrade .NET 5.0 Project to .NET 9.0

--- Demo_ProjectStructureNet5/Program.cs
+++ Demo_ProjectStructureNet5/Program.cs
@@ -2,25 +2,74 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Demo_ProjectStructureNet5
 {
+
+    ///.NET 5.0 version - There are program file and startup file.
+    ///public class Program
+    ///{
+    ///    //Entry Point
+    ///    public static void Main(string[] args)
+    ///    {
+    ///        CreateHostBuilder(args).Build().Run();
+    ///    }
+    ///
+    ///    public static IHostBuilder CreateHostBuilder(string[] args) =>//This Function Build/Create The Host "Kestrel"
+    ///        Host.CreateDefaultBuilder(args)
+    ///            .ConfigureWebHostDefaults(webBuilder =>
+    ///            {
+    ///                webBuilder.UseStartup<Startup>();//CreateHostBuilder() take Configuration of this host "Kestrel" from this class "Startup".
+    ///            });
+    ///}
+
+    //Upgrade to.NET 9.0 - There is only program file which contain code of startup file.
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //1. Create HostBuilder "Kestrel" -> var WebApplicationBuilder = WebApplication.CreateBuilder();
+            //2. Configure Services of the Host "Kestrel" -> WebApplicationBuilder.Services.AddControllersWithViews();
+            //3. Build This HostBuilder to return the "Kestrel" to configure it's pipeline -> var app = WebApplicationBuilder.Build();
+            //4. Configure The WebApp "Kestrel" Environment and Request Pipeline.
+            //5. Run the WebApp -> app.Run()
+
+            var WebApplicationBuilder = WebApplication.CreateBuilder();//Instead of function "CreateHostBuilder()"
+
+            #region Configure Services
+
+            WebApplicationBuilder.Services/*DI Container*/.AddControllersWithViews();
+
+            #endregion
+
+            var app = WebApplicationBuilder.Build();//Build the WebApplicationBuilder.
+
+            #region Configure HTTP Request Pipeline.
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+            #endregion
+
+            app.Run();
+
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
--- Demo_ProjectStructureNet5/Startup.cs
+++ Demo_ProjectStructureNet5/Startup.cs
@@ -16,17 +16,26 @@ namespace Demo_ProjectStructureNet5
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            ////Inside This function we add services which we need in the project to the Dependency injection Container.
+            ////This Container is in "Kestrel" Console App for services use the Dependency injection.
+            ////So any service you need to make it work with DI Approach, put it inside this function "ConfigureServices()"
+            ////services.AddMvc();//Add Controllers - Views - Razor Pages - API [Work in App with MVC & RazorPages & API]
+            //services.AddMvcCore();//Add Controllers [Add Minimal essential MVC services ]
+            //services.AddRazorPages ();//Add Views - RazorPages [Work in App with "RazorPages" only ]
+            //services.AddControllers();//Support API Controllers [Work in App with "API" only ]
+            services.AddControllersWithViews();//Add MVC Controllers [Work in App with "MVC" only] but support API Controllers
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Configure the middlewares that will applied on the HTTP request.
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();//Middleware for errors
             }
 
-            app.UseRouting();
+            app.UseRouting();//Middleware for routing [Tell the request the way that it will pass through based on the routing table [EndPoints table]]
 
             app.UseEndpoints(endpoints =>
             {
@@ -34,7 +43,7 @@ namespace Demo_ProjectStructureNet5
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+            });//Middleware for Endpoint route mapping.
         }
     }
 }
